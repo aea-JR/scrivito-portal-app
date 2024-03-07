ef0a53070fc8bde2f2fbc5d9f8b0c26b31bb7bd8
Alexander Pepper <alexander.pepper@justrelate.com>
2024-03-07 17:25:12 +0100

    INTERMEDAITE: USE PATCH INSTEAD OF PUT - FOR NOW

--- src/Data/Gdpr/GdprDataClass.ts
+++ src/Data/Gdpr/GdprDataClass.ts
@@ -1,8 +1,36 @@
-import { provideDataClass } from 'scrivito'
+import { provideDataClass, unstable_JrRestApi } from 'scrivito'
 
 const apiPath = '../pisa-api/gdpr'
 
 export const Gdpr = provideDataClass('Gdpr', {
-  // @ts-expect-error until out of private beta
-  apiPath,
+  connection: {
+    index: async (params) =>
+      unstable_JrRestApi.fetch(apiPath, {
+        params: {
+          ...params.filters(),
+          _continuation: params.continuation(),
+          _order: params.order().length
+            ? params
+                .order()
+                .map((o) => o.join('.'))
+                .join(',')
+            : undefined,
+          _limit: params.limit().toString(),
+          _search: params.search() || undefined,
+        },
+      }) as Promise<{ results: Array<{ _id: string }>; continuation?: string }>,
+
+    get: async (id) => {
+      const response = await unstable_JrRestApi.fetch(`${apiPath}/${id}`)
+      return response
+    },
+
+    update: async (id, data) => {
+      const response = await unstable_JrRestApi.fetch(`${apiPath}/${id}`, {
+        method: 'put',
+        data,
+      })
+      return response
+    },
+  },
 })
