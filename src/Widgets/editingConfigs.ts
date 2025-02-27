cb2bd6444457c155071e54ca7e778c3aa70bb7fb
Amier El Ahmad <amier.elahmad@justrelate.com>
2025-02-27 13:46:56 +0100

    Questionnaire package

--- src/Widgets/editingConfigs.ts
+++ src/Widgets/editingConfigs.ts
@@ -1,8 +1,10 @@
 import { loadEditingConfigs } from 'scrivito-neoletter-form-widgets/editing'
+import { loadQuestionnaireEditingConfigs } from 'psa-scr-qst-tst/editing'
 
 import.meta.glob(['./**/*EditingConfig.ts', './**/*EditingConfig.tsx'], {
   eager: true,
 })
+loadQuestionnaireEditingConfigs()
 loadEditingConfigs()
 
 export {}
--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -1,7 +1,9 @@
 import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
+import { initPisaQuestionnaireWidgets } from "psa-scr-qst-tst";
 import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
 
+initPisaQuestionnaireWidgets({pisaUrl: "https://web130.crm.pisasales.de/api-salesportal"})
 initNeoletterFormWidgets()
 export {}
